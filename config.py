a6e8c5076b5765a2300fca05864d4017662c1d43
Badnam xd <yadavxd746@gmail.com>
2025-02-26 11:23:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AHarshit14363/BABY-MUSIC",
+    "https://github.com/Badnam019/Sheru-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
